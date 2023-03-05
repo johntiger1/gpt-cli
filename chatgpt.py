cec6dea1fbdfbe8d52b1c793e2ab4ee777e0cf3e
John Chen <johnchen@lyft.com>
2023-03-04 21:34:24 -0500

    prompt engineer

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import re
 
 # Set up OpenAI API credentials
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -25,16 +26,38 @@ def generate_respone_v2(prompt):
     import os
     import openai
     openai.api_key = os.getenv("OPENAI_API_KEY")
-
+    pattern = "___code____"
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "Hello!" + prompt}
+            {"role": "system", "content": "You are codeGPT. You return runnable code only. Produce"
+                                          "an error message if appropriate.}"},
+            {"role": "user", "content": f"Produce ONLY executable code for the following prompt. Do not include any explanation or natural language"
+                                        # f"Give me a runnable python program for the following question. "
+                                        # f""
+                                        # f"Surround ONLY the "
+                                        # f"executable code block with {pattern} before and after; i.e. make sure that what is surrounded "
+                                        # f"by {pattern}  is executable: "
+                                        + prompt}
             # {"role": "user", content: "" }
         ]
     )
+    re_pattern = fr"{pattern}\s*(.*?)\s*{pattern}"
+    match = re.search(re_pattern, str(completion.choices[0].message), re.DOTALL)
 
-    print(completion.choices[0].message)
+    # Code to extract the command
+    if match:
+        code = match.group(1)
+        print('code extracted')
+        print("")
+        print("this is the extracted code",code.strip())
+
+    # You: write me an endpoint for a flask POST request that accepts image upload, with a ratelimiter
+    # Sanitization layer
+
+    # Command to execute the command
+
+    print(str(completion.choices[0].message.content))
     # return completion.choices[0].message
 
 
@@ -43,7 +66,7 @@ while True:
     user_input = input("You: ")
     if user_input.lower() == "exit":
         break
-    prompt += "\nUser: " + user_input
+    prompt = "\nUser: " + user_input
     response = generate_respone_v2(prompt)
     # print("ChatGPT: " + response)
 
