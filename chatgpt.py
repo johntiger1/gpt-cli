ff38582dc1c2f9f5e99fafda5c617f4ed0ccaaa9
roustemkhamitov <rustem.traveldesk@gmail.com>
2023-03-09 20:24:46 +0400

    Clean up

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,7 @@
 import openai
 import os
 import re
+import subprocess
 
 # Set up OpenAI API credentials
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -18,22 +19,7 @@ history = [
         CONTENT_DICT]
 # Define a function to generate a response
 def generate_response(prompt):
-    response = openai.ChatCompletion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    message = response.choices[0].text.strip()
-    return message
-
-def generate_respone_v2(prompt):
-    import os
-    import openai
     global history
-    openai.api_key = os.getenv("OPENAI_API_KEY")
     pattern = "___code____"
     new_message = CONTENT_DICT
     new_message["content"] += prompt
@@ -41,22 +27,12 @@ def generate_respone_v2(prompt):
         model="gpt-3.5-turbo",
         messages=history
     )
-    re_pattern = fr"{pattern}\s*(.*?)\s*{pattern}"
-    match = re.search(re_pattern, str(completion.choices[0].message), re.DOTALL)
-
-    # Code to extract the command
-    if match:
-        code = match.group(1)
-        print('code extracted')
-        print("")
-        print("this is the extracted code",code.strip())
-
     # Command to execute the command
-    print(str(completion.choices[0].message.content))
     system_output_response = completion.choices[0].message.content
     output_dict = {"role":"assistant", "content":f"{system_output_response}"}
     history.append(output_dict)
     history = history + history[:2]
+    return system_output_response
 
 
 # Get input from the user and generate a response
@@ -65,4 +41,12 @@ while True:
     if user_input.lower() == "exit":
         break
     prompt = "\nUser: " + user_input
-    response = generate_respone_v2(prompt)
+    print("\n")
+    response = str(generate_response(prompt))
+    print("The generated command that will be run is:\n")
+    print(response)
+    user_input = input("Execute Command?(Y/N) ")
+
+    if user_input.lower() == "y":
+         os.system(response)
+    print("\n")
